5325e43be0ed02e134a1013cabee722bc462d318
Mr KE <ernestkouassi02@gmail.com>
2020-11-04 09:59:51 +0000

    Mise a jour

--- js/app.js
+++ js/app.js
@@ -4,4 +4,26 @@ $(function () {
   $("a[data-activates='mobile-menu']").on('click', function () {
       $("#mobile-menu").toggle();
   });
+
+  $(".select2").select2();
+
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    autoplay: true,
+    autoplayHoverPause: true,
+    loop: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+
+  });
 });
